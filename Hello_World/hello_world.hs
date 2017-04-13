ac2ddd30646be5ee658beb0d161cc608287c37ae
George Sparrow <sparrowg@iusb.edu>
2017-04-12 19:27:34 -0600

    Initial Commit
    Begins with Hello World
    Adopts coding standard set forth by Penn Engineering School at the following link
    http://www.seas.upenn.edu/~cis552/12fa/styleguide.html
    Adopts 4 spaces instead of tabs.

---
+++ Hello_World/hello_world.hs
@@ -0,0 +1,9 @@
+-- | vim: filetype=haskell expandtab shiftwidth=4 softtabstop=4 foldmethod=marker
+-- | This program is a simple Hello World
+{-# OPTIONS_GHC -Wall -fwarn-tabs #-}
+
+-- | Outputs "Hello World"
+-- | main :: IO ()
+main::IO()
+main = putStrLn "Hello World"
+
